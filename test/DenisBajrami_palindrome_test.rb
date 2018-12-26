c8ba777a47639989e33b20d0e9638d0ed5eb9b9b
Denis Bajrami <denis.sec773@gmail.com>
2018-12-26 13:06:41 +0100

    Test-driven development setup

--- test/DenisBajrami_palindrome_test.rb
+++ test/DenisBajrami_palindrome_test.rb
@@ -3,19 +3,4 @@ require "test_helper"
 class DenisBajramiPalindromeTest < Minitest::Test
 
 
-# Returns true for a palindrome, false otherwise.
-  def palindrome?
-    processed_content == processed_content.reverse
-  end
-
-# Returns the letters in the string.
-  def letters
-  end
-
-  private
-
-# Returns content for palindrome testing.
-  def processed_content
-    self.downcase
-  end
 end
